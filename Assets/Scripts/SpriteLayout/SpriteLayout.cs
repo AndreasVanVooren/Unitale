199c100278f089270746f4437e985f3be2224a44
Andreas Van Vooren (N00bFlesh) <andreas.van.vooren@gmail.com>
2016-04-28 23:34:46 +0200

    Made a semi-functional sprite layout system.

--- Assets/Scripts/SpriteLayout/SpriteLayout.cs
+++ Assets/Scripts/SpriteLayout/SpriteLayout.cs
@@ -1,158 +1,62 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.Collections;
 
-public enum ColliderType
+namespace SpriteLayout
 {
-	Rect,
-	Circle,
-}
-
-[CustomEditor(typeof(Transform))]
-public class TransformSpriteLayout : Editor
-{
-	public override void OnInspectorGUI()
+	public enum ColliderType
 	{
-		if (((Transform)target).GetComponentInParent<SpriteLayout>() != null)
-		{
-			EditorGUILayout.HelpBox("Controlled by SpriteLayout\n(Although you can tweak it in the debug inspector)",MessageType.Warning);
-			return;
-		}
-		base.OnInspectorGUI();
+		Rect,
+		Circle,
 	}
-}
 
-[CustomEditor(typeof(SpriteLayout))]
-public class CustSpriteRendInspector : Editor
-{
-	public override void OnInspectorGUI()
+	[RequireComponent(typeof(SpriteRenderer))]
+	[ExecuteInEditMode]
+	public class SpriteLayout : SpriteLayoutBase
 	{
-		base.OnInspectorGUI ();
+		private SpriteRenderer _renderer;
+		private Sprite _mySprite;
 
-		var tgt = (SpriteLayout)target;
-
-		if (tgt.transform.parent == null || tgt.transform.GetComponentInParent<SpriteLayout>() == null) 
+		protected override void Initialize()
 		{
-			EditorGUILayout.LabelField("Needs parent sprite for anchoring");
-		} 
-		else 
-		{
-			tgt.Anchor = EditorGUILayout.Vector2Field ("Anchor", tgt.Anchor);
+			base.Initialize();
+			_renderer =GetComponent<SpriteRenderer>();
+			_mySprite = _renderer.sprite;
+			_initialDimensions = Dimensions = _mySprite.bounds.size;
 		}
-		tgt.Pivot = EditorGUILayout.Vector2Field ("Pivot", tgt.Pivot);
-	}
-}
 
-[RequireComponent(typeof(SpriteRenderer))]
-[ExecuteInEditMode]
-public class SpriteLayout : MonoBehaviour
-{
-	private SpriteLayout _parent;
+		// Use this for initialization
+		void Awake()
+		{
+			Initialize();
+			//transform.
+		}
 
-	private Vector3 _localPosition;
-
-	private SpriteRenderer _renderer;
-	private Sprite _mySprite;
-
-	/// <summary>
-	/// The anchor, aka where this sprite attaches to on its parent
-	/// </summary>
-	private Vector2 _anchor = new Vector2(0.5f,0.5f);
-	public Vector2 Anchor
-	{
-		get { return _anchor; }
-		set 
-		{ 
-			if (_anchor == value)
-				return;
-			var pSprite = GetComponentInParent<SpriteLayout>();
-			if (pSprite == null)
+		public void AttachCollider(ColliderType type)
+		{
+			switch (type)
 			{
-				//Debug.Log("parent null");
-				return;
+				case ColliderType.Rect:
+					{
+						var collider = this.gameObject.AddComponent<BoxCollider2D>();
+						collider.size = Dimensions;
+						break;
+					}
+				case ColliderType.Circle:
+					{
+						var collider = this.gameObject.AddComponent<CircleCollider2D>();
+						float avg = (Dimensions.x + Dimensions.y) / 2;
+						collider.radius = avg;
+						break;
+					}
+				default:
+					break;
 			}
-
-			Vector3 anch = _anchor;
-			anch.x -= 0.5f;
-			anch.y -= 0.5f;
-			anch.Scale( pSprite._mySprite.bounds.extents );
-
-			Vector3 diff = transform.localPosition - anch ;
-			//Debug.Log(diff);
-			_anchor = value; 
-
-			anch = value;
-			anch.x -= 0.5f;
-			anch.y -= 0.5f;
-			anch.Scale( pSprite._mySprite.bounds.extents );
-
-			transform.localPosition = diff + anch;
 		}
-	}
 
-	/// <summary>
-	/// The pivot.
-	/// </summary>
-	private Vector2 _pivot = new Vector2(0.5f,0.5f);
-	public Vector2 Pivot
-	{
-		get { return _pivot; }
-		set 
-		{ 
-			//_mySprite.pivot = value;
-			_pivot = value; 
-		}
-	}
-	// Use this for initialization
-	void Awake ()
-	{
-		_renderer = GetComponent<SpriteRenderer> ();
-		_mySprite = _renderer.sprite;
-		_parent = GetComponentInParent<SpriteLayout>();
-		//transform.
-	}
-
-	public void AttachCollider(ColliderType type)
-	{
-		switch (type) 
+		// Update is called once per frame
+		void Update()
 		{
-			case ColliderType.Rect:
-				{
-					var collider = this.gameObject.AddComponent<BoxCollider2D>();
-					collider.size = _mySprite.bounds.size;
-					break;
-				}
-			case ColliderType.Circle:
-				{
-					var collider = this.gameObject.AddComponent<CircleCollider2D>();
-					float avg = (_mySprite.bounds.size.x + _mySprite.bounds.size.y) / 2;
-					collider.radius = avg;
-					break;
-				}
-			default:
-				break;
+			//transform.localPosition = _localPosition;
 		}
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-		//transform.localPosition = _localPosition;
-	}
-
-	public void MoveTo(Vector2 vec)
-	{
-		if (_parent == null)
-		{
-			transform.localPosition = (Vector3)vec;
-			return;
-		}
-
-		Vector3 anch = _anchor;
-		anch.x -= 0.5f;
-		anch.y -= 0.5f;
-		anch.Scale( _parent._mySprite.bounds.extents );
-
-		transform.localPosition = anch + (Vector3)vec;
 	}
 }
---
+++ Assets/Scripts/SpriteLayout/SpriteLayoutBase.cs
@@ -0,0 +1,294 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace SpriteLayout
+{
+	[CustomEditor(typeof(Transform))]
+	public class SpriteLayoutTransformInspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var t = (Transform)target;
+
+			var spr = t.GetComponent<SpriteLayoutBase>();
+
+			if (spr != null)
+			{
+				EditorGUILayout.HelpBox("These are SpriteLayout controls,\nfor actual transform controls, check the debug inspector", MessageType.Warning);
+
+				spr.LocalPosition = EditorGUILayout.Vector3Field("Position", spr.LocalPosition);
+				spr.LocalRotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", spr.LocalRotation.eulerAngles));
+				spr.LocalScale = EditorGUILayout.Vector3Field("Scale", spr.LocalScale);
+
+				spr.Width =  EditorGUILayout.FloatField("Width", spr.Width);
+				spr.Height = EditorGUILayout.FloatField("Height", spr.Height);
+
+				var parents = spr. GetComponentsInParent<SpriteLayoutBase>();
+				
+				if(spr.Parent == null || t.parent == null || parents.Length <= 1)
+				{
+					if(t.GetComponentInParent<SpriteLayoutBase>() == spr)
+					{
+						Debug.Log(t.GetComponentInParent<SpriteLayoutBase>().GetInstanceID());
+						Debug.Log(spr.GetInstanceID());
+					}
+					EditorGUILayout.LabelField("Needs parent for anchoring");
+				}
+				else
+				{
+					spr.Anchor = EditorGUILayout.Vector2Field("Anchor", spr.Anchor);
+				}
+				spr.Pivot = EditorGUILayout.Vector2Field("Pivot", spr.Pivot);
+
+				return;
+			}
+
+			t.localPosition = EditorGUILayout.Vector3Field("Position", t.localPosition);
+			t.localRotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", t.localRotation.eulerAngles));
+			t.localScale = EditorGUILayout.Vector3Field("Scale", t.localScale);
+			//DrawDefaultInspector();
+		}
+	}
+
+	[CustomEditor(typeof(SpriteLayoutBase))]
+	public class SpriteLayoutBaseInspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			var tgt = (SpriteLayoutBase)target;
+
+			if (tgt.transform.parent == null || tgt.transform.GetComponentInParent<SpriteLayoutBase>() == null)
+			{
+				EditorGUILayout.LabelField("Needs parent sprite for anchoring");
+			}
+			else
+			{
+				tgt.Anchor = EditorGUILayout.Vector2Field("Anchor", tgt.Anchor);
+			}
+			tgt.Pivot = EditorGUILayout.Vector2Field("Pivot", tgt.Pivot);
+		}
+	}
+
+	[ExecuteInEditMode]
+	public class SpriteLayoutBase : MonoBehaviour
+	{
+		//protected delegate void OnPropertyChangeLocPos();
+		//protected delegate void OnPropertyChangeLocRot();
+		//protected delegate void OnPropertyChangeLocScale();
+		//protected delegate void OnPropertyChangeWidth();
+		//protected delegate void OnPropertyChangeHeight();
+		//protected delegate void OnPropertyChangeAnch();
+		//protected delegate void OnPropertyChangePiv();
+
+		private Vector3 _localPosition;
+		public Vector3 LocalPosition
+		{
+			get { return _localPosition; }
+			set
+			{
+
+				_localPosition = value;
+
+				transform.localPosition = value + (Vector3)NormalizedAnchor;
+			}
+		}
+
+		private Quaternion _localRotation;
+		public Quaternion LocalRotation
+		{
+			get { return _localRotation; }
+			set
+			{
+				_localRotation = value;
+
+				Vector2 dir = _localRotation * NormalizedPivot;
+
+				transform.localPosition = (Vector3)(NormalizedAnchor - dir) + LocalPosition;
+			}
+		}
+
+		private Vector3 _localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		public Vector3 LocalScale
+		{
+			get { return _localScale; }
+			set
+			{
+				_localScale = value;
+
+				//transform.localPosition = 
+			}
+		}
+
+		//private SpriteRenderer _renderer;
+		//private Sprite _mySprite;
+
+		protected Vector2 _initialDimensions = new Vector2(1, 1);
+		private Vector2 _dimensions = new Vector2(1, 1);
+		public Vector2 Dimensions
+		{
+			get { return _dimensions; }
+			set
+			{
+				_dimensions = value;
+			}
+		}
+
+		public Vector2 Extents
+		{
+			get { return Dimensions / 2; }
+		}
+
+		public float Width
+		{
+			get { return Dimensions.x; }
+			set 
+			{ 
+				var scale = transform.localScale;
+				scale.x = value / _initialDimensions.x;
+				transform.localScale = scale;
+				Dimensions = new Vector2(value, _dimensions.y);
+			}
+		}
+
+		public float Height
+		{
+			get { return Dimensions.y; }
+			set 
+			{
+				var scale = transform.localScale;
+				scale.y *= value / _initialDimensions.y;
+				transform.localScale = scale;
+				Dimensions = new Vector2(_dimensions.x, value); 
+			}
+		}
+
+		/// <summary>
+		/// The anchor, aka where this sprite attaches to on its parent
+		/// </summary>
+		private Vector2 _anchor = new Vector2(0.5f, 0.5f);
+		public Vector2 Anchor
+		{
+			get { return _anchor; }
+			set
+			{
+				if (_anchor == value)
+					return;
+				var pSprite = GetComponentInParent<SpriteLayout>();
+				if (pSprite == null)
+				{
+					//Debug.Log("parent null");
+					return;
+				}
+
+				Vector3 anch = NormalizedAnchor;
+
+				Vector3 diff = transform.localPosition - anch;
+				//Debug.Log(diff);
+				_anchor = value;
+
+				anch = NormalizedAnchor;
+
+				transform.localPosition = diff + anch;
+			}
+		}
+
+		public Vector2 NormalizedAnchor
+		{
+			get
+			{
+				var pSprite = Parent;
+				if(pSprite == null)
+				{
+					var parents = GetComponentsInParent<SpriteLayoutBase>();
+					if (parents.Length > 1)
+						pSprite = parents[1];
+					else return Vector2.zero;
+				}
+
+				Vector2 anch = _anchor;
+
+				anch.x -= 0.5f;
+				anch.y -= 0.5f;
+				anch.Scale(pSprite.Dimensions);
+				return anch;
+
+			}
+		}
+
+		/// <summary>
+		/// The pivot.
+		/// </summary>
+		private Vector2 _pivot = new Vector2(0.5f, 0.5f);
+		public Vector2 Pivot
+		{
+			get { return _pivot; }
+			set
+			{
+				//_mySprite.pivot = value;
+				//if (_mySprite != null)
+				{
+					Vector2 diff = value - _pivot;
+					//diff.x -= 0.5f;
+					//diff.y -= 0.5f;
+					diff.Scale(Dimensions);
+					diff.Scale(LocalScale);
+					diff = LocalRotation * diff;
+
+					Vector3 pos = transform.localPosition + (Vector3)diff;
+					transform.localPosition = pos;
+				}
+
+				_pivot = value;
+			}
+		}
+
+		public Vector2 NormalizedPivot
+		{
+			get
+			{
+				//if (_mySprite != null)
+				{
+					Vector2 piv = _pivot;
+
+					piv.x -= 0.5f;
+					piv.y -= 0.5f;
+					piv.Scale(Dimensions);
+					return piv;
+				}
+				//return Vector2.zero;
+
+			}
+		}
+
+		public SpriteLayoutBase Parent
+		{ get; protected set; }
+
+
+		protected virtual void Initialize()
+		{
+			if(transform.parent == null)
+			{
+				Debug.Log("NO PARENTS!");
+			}
+			var parents = GetComponentsInParent<SpriteLayoutBase>();
+			if (parents.Length > 1)	//check if there is a gameobject.
+				Parent = parents[1];
+			if (Parent == this) Parent = null;
+		}
+
+		// Use this for initialization
+		void Awake()
+		{
+			Initialize();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
